0556626e6a57cddc1a86ddd9e920fa1c2b313547
Slana958 <svetlanaisraelyan97gmail.com>
2025-11-17 13:26:14 +0400

    add project to git

--- index.php
+++ index.php
@@ -209,7 +209,7 @@
         echo "<br>";
 
         $keys=["name","age","city"];
-        $values=["john",25,"Yerevan"];
+        $values=["john",25,"Yerevan"]; 
         $array=array_combine($keys,$values);
         print_r($array);
         echo "<br>";
@@ -218,13 +218,126 @@
          echo "<br>";
          
          print_r(array_splice($array,1));//14-rd
+         echo "<br>";
 
+         $nameErr = $emailErr = $genderErr = $websiteErr = "";
+         $name = $email = $gender = $comment = $website = "";
 
-        
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+         $nameErr = "Name is required";
+      } else {
+    $name = test_input($_POST["name"]);
+  }
+  if (empty($_POST["lname"])) {
+         $lnameErr = "last name is required";
+      } else {
+    $lname = test_input($_POST["lname"]);}
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+  if (empty($_POST["password"])) {
+    $passwordErr = "Password is required";
+  } else {
+    $password = test_input($_POST["password"]);
+  }
+    
+  if (empty($_POST["valid"])) {
+    $validErr = "";
+  } else {
+    $valid = test_input($_POST["valid"]);
+  }
 
-          
-          
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
 
+  if (empty($_POST["gender"])) {
+    $genderErr = "";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+  if(empty ($_POST["username"])){
+    $usernameErr = "";
+  }else{
+    $username= test_input($_POST["username"]);
+  }
+  if (empty($_POST["tel"])) {
+    $telErr= "";
+  } else {
+    $tel = test_input($_POST["tel"]);
+  }
+  if (empty($_POST["date"])) {
+    $dateErr= "";
+  } else {
+    $date = test_input($_POST["date"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  Lastname: <input type="text" name="lname">
+  <span class="error">* <?php echo $lnameErr;?></span>
+  <br><br>
+  Username: <input type="text" name="username">
+  <span class="error">* <?php echo $usernameErr;?></span>
+  <br><br>
+  
+  E-mail: <input type="text" name="email">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Password: <input type="password" name="password">
+  <span class="error">* <?php echo $passwordErr;?></span>
+  <br><br>
+  Validation of password <input type="password" name="valid">
+  <span class="error"><?php echo $validErr;?></span>
+  <br><br>
+  Address: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  tell: <input type="tel" name="tel">
+  <span class="error">* <?php echo $telErr;?></span>
+  <br><br>
+  Date: <input type="date" name="date">
+  <span class="error">* <?php echo $dateErr;?></span>
+  <br><br>
+  
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
  ?>
 
     </body>
